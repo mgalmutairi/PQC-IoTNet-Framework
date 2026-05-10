40aeff2ebe039e847e38e335248816de28061cfa
mgalmutairi <153891823+mgalmutairi@users.noreply.github.com>
2026-05-10 00:49:32 -0700

    Add files via upload

---
+++ deps/liboqs/build/CMakeFiles/prettyprint.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prettyprint"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/prettyprint.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
